60b9ed2db15a9e004e9cb341335fa33c01d182e8
SaKu <sakuofficial@gmail.com>
2019-10-23 22:33:40 +0530

    add changes to js file

--- timer.js
+++ timer.js
@@ -3,7 +3,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-p {
+p{
   text-align: center;
   font-size: 60px;
   margin-top: 0px;
@@ -22,10 +22,10 @@ var countDownDate = new Date("Jan 5, 2019 15:37:25").getTime();
 var x = setInterval(function() {
 
     // Get todays date and time
-    var now = new Date().getTime();
+    var current = new Date().getTime();
     
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+    // Find the distance between current and the count down date
+    var distance = countDownDate - current;
     
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -38,9 +38,9 @@ var x = setInterval(function() {
     + minutes + "m " + seconds + "s ";
     
     // If the count down is over, write some text 
-    if (distance < 0) {
+    if (distance <= 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("demo").innerHTML = "oops..! EXPIRED!";
     }
 }, 1000);
 </script>
