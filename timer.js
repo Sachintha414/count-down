5fbebbe006f82f51c115e285d636c6bea1ab1994
SaKu <sakuofficial@gmail.com>
2019-10-23 22:39:14 +0530

    change variable name in minutes

--- timer.js
+++ timer.js
@@ -27,15 +27,15 @@ var x = setInterval(function() {
     // Find the distance between current and the count down date
     var distance = countDownDate - current;
     
-    // Time calculations for days, hours, minutes and seconds
+    // Time calculations for days, hours, mins and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Output the result in an element with id="demo"
     document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    + mins + "m " + seconds + "s ";
     
     // If the count down is over, write some text 
     if (distance <= 0) {
